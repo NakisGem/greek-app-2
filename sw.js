7976343940247b0a908b9096caf958d246dda18b
NakisGem <106708309+NakisGem@users.noreply.github.com>
2026-04-07 06:41:28 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'greek-en-v2';
+const FILES = ['./index.html', './manifest.json', './sw.js', './icon.svg'];
+
+self.addEventListener('install', e =>
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)))
+);
+
+self.addEventListener('activate', e =>
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  )
+);
+
+self.addEventListener('fetch', e =>
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  )
+);
